5e655e6505e8c954e8426a82b865aa06c643187f
Luan Lopes <lopesluan18@outlook.com>
2022-05-15 20:16:41 -0300

    Tentativa de condicional para senha e Mostrar/Ocultar senha

--- Receitas/animation.js
+++ Receitas/animation.js
@@ -1,14 +1,33 @@
-ativar.addEventListener('click', () =>{
-    modal.classList.toggle('animar');
-});
+
 
 function clicar(){
 let enviar = window.document.getElementById('enviar')
 let load = window.document.getElementById('load')
+let usuario = window.document.getElementById('usuario')
+let senha = window.document.getElementById('senha')
 load.style.display = 'block'
 enviar.style.display = 'none'
-setTimeout(function(){
-window.location.href="Home.html"
-},3000);
-}
+if(usuario == 'sangbom' || senha == 1234){
+        setTimeout(function(){
+        window.alert('Login realizado com sucesso !!!')
+        window.location.href="Home.html"
+        },3000);
+        }
+ else{
+        setTimeout(function(){
+        window.alert('Usuario ou senha incorretas !!!')
+        window.location.href="SenhaInvalida.html"
+        },3000);
+        }
+ }       
 
+function mostrar(){
+let mostrar = window.document.getElementById('mostrar')
+let password = window.document.getElementById('senha')
+if(senha.type == 'password'){
+  senha.type='text'
+}
+else{
+  senha.type='password'  
+}
+}
