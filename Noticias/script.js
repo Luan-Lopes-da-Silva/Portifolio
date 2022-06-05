709f17d4adf03126b838ca10f518772e3eca3eb9
Luan Lopes <lopesluan18@outlook.com>
2022-06-04 23:39:31 -0300

    Ideia de menu Dropdown inicial

---
+++ Noticias/script.js
@@ -0,0 +1,25 @@
+let menudropdown = document.getElementById('dropdown')
+let box = document.getElementsByClassName('dropdown')
+
+
+
+
+function Dropdown(){
+    menudropdown.style.backgroundColor = '#C1C1F4'  
+    menudropdown.style.color = 'black'  
+box[0].style.display = 'block';
+for (let box in boxs){
+  box.style.display = 'block'; 
+ 
+}
+}
+
+function Sumir(){
+    menudropdown.style.color = '#C1C1F4'  
+    menudropdown.style.backgroundColor = 'transparent'  
+    box[0].style.display = 'none';
+    for (let box in boxs){
+      box.style.display = 'none';   
+    }  
+}
+
