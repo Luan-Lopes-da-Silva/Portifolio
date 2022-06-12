c6b31f442b5423dd739e3787260919e35417795e
Luan Lopes <lopesluan18@outlook.com>
2022-06-12 20:21:08 -0300

    Refatoração de grande parte do site usando Sass

---
+++ Receitas/Login/animation.js
@@ -0,0 +1,54 @@
+//VARIAVEIS//
+let slide = document.getElementById('animar')
+let box = document.getElementById('box')
+let olho = document.getElementById('mostrar')
+let btn = document.getElementById('enviar')
+let load = document.getElementById('load')
+let senha = document.getElementById('senha')
+
+
+//FIM//
+
+
+
+//Animação entrada de Form login//
+slide.addEventListener('click', () =>{
+box.classList.toggle('animar')
+})
+//FIM//
+
+
+
+//Função mostrar/Ocultar senha//
+function mostrar(){
+if(senha.type == 'password'){
+    senha.type = 'text'
+}else{
+senha.type = 'password'
+}    
+}
+//FIM//
+
+//Função ao clicar carrega load//
+function clicar(){
+btn.style.display='none'
+load.style.display='block'
+}
+
+//Squares//
+let ulSquares = document.querySelector('ul.squares')
+for(let i=0; i<11; i++){
+const li = document.createElement("li")
+const size = Math.floor(Math.random()*(120-10)+10);
+const position = Math.random() * (99-1)+1;
+const delay =Math.random() * (5,0.1)+0.1;
+const duration =Math.random() * (24,12)+12;
+li.style.width =`${size}px`
+li.style.height =   `${size}px`
+li.style.bottom =   `${size}px`
+li.style.left = `${position}%`
+li.style.animationDelay =`${delay}s`
+li.style.animationDuration =`${duration}s`
+li.style.animationTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()}) `
+ulSquares.appendChild(li)
+}
