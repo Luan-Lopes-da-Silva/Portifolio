5d3683c1d3364479c6ff9d32e8463661c9e94293
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-05-18 21:18:12 -0300

    Pagina home feita com algumas funções

--- Financas/Code/Scripts/home.js
+++ Financas/Code/Scripts/home.js
@@ -18,4 +18,40 @@ function hello(){
  }
 }
 
+const valorSpan = document.querySelector('.saldo span')
+const numeroConvertido = parseFloat(valorSpan.innerText.slice(3).replace(',','.'))
+
+if(numeroConvertido<=19){
+valorSpan.style.color = '#FF0000'
+}else if(numeroConvertido>=20 && numeroConvertido<=80){
+valorSpan.style.color = '#EBFF00'
+}else if(numeroConvertido>80){
+valorSpan.style.color = '#9EEF41'
+}
+
+const spanCartoes = document.querySelectorAll('.cartao .saldo')
+spanCartoes.forEach((span)=>{
+const spanConvertido =  parseFloat(span.innerText.slice(3).replace(',','.'))
+if(spanConvertido<=19){
+  span.style.color = '#FF0000'
+  }else if(spanConvertido>=20 && spanConvertido<=80){
+  span.style.color = '#EBFF00'
+  }else if(spanConvertido>80){
+  span.style.color = '#9EEF41'
+  }
+})
+
+const spanOperacao = document.querySelector('.operacoes .situacao')
+const situacao = parseFloat(spanOperacao.innerText.slice(0,2))
+if(situacao<=20){
+  spanOperacao.nextElementSibling.style.display = 'none'
+  spanOperacao.style.color = '#9EEF41'
+}else if(situacao>20 && situacao<=60){
+  spanOperacao.nextElementSibling.style.display = 'none'
+  spanOperacao.style.color = '#EBFF00'
+}else{
+  spanOperacao.nextElementSibling.style.display = 'block'
+  spanOperacao.style.color = '#FF0000'
+}
+
 hello();
