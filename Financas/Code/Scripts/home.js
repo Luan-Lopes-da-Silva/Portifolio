6abf559169c3b7b6f10ffb95ad88d00f7eedae04
Luan-Lopes-da-Silva <lopesluan18@outlook.com>
2023-05-22 21:00:54 -0300

    Algumas refatorações

--- Financas/Code/Scripts/home.js
+++ Financas/Code/Scripts/home.js
@@ -8,50 +8,16 @@ function hello(){
  const hours = date.getHours()
  const minutes = date.getMinutes()
  if(hours>6 && hours<13){
-  sayHello.innerText = `Bom dia ${objReal.nome}`
+  sayHello.innerText = `Bom dia ${objReal.usuario}`
  }else if(hours>13 && hours<=18){
-  sayHello.innerText = `Boa tarde ${objReal.nome}`
+  sayHello.innerText = `Boa tarde ${objReal.usuario}`
   sayHello.style.width = 300+'px'
  }else{
-  sayHello.innerText = `Boa noite ${objReal.nome}`
+  sayHello.innerText = `Boa noite ${objReal.usuario}`
   sayHello.style.width = 300+'px'
  }
 }
 
-const valorSpan = document.querySelector('.saldo span')
-const numeroConvertido = parseFloat(valorSpan.innerText.slice(3).replace(',','.'))
 
-if(numeroConvertido<=19){
-valorSpan.style.color = '#FF0000'
-}else if(numeroConvertido>=20 && numeroConvertido<=80){
-valorSpan.style.color = '#EBFF00'
-}else if(numeroConvertido>80){
-valorSpan.style.color = '#9EEF41'
-}
-
-const spanCartoes = document.querySelectorAll('.cartao .saldo')
-spanCartoes.forEach((span)=>{
-const spanConvertido =  parseFloat(span.innerText.slice(3).replace(',','.'))
-if(spanConvertido<=19){
-  span.style.color = '#FF0000'
-  }else if(spanConvertido>=20 && spanConvertido<=80){
-  span.style.color = '#EBFF00'
-  }else if(spanConvertido>80){
-  span.style.color = '#9EEF41'
-  }
-})
-
-const spanOperacao = document.querySelector('.operacoes .situacao')
-const situacao = parseFloat(spanOperacao.innerText.slice(0,2))
-if(situacao<=20){
-  spanOperacao.nextElementSibling.style.display = 'none'
-  spanOperacao.style.color = '#9EEF41'
-}else if(situacao>20 && situacao<=60){
-  spanOperacao.nextElementSibling.style.display = 'none'
-  spanOperacao.style.color = '#EBFF00'
-}else{
-  spanOperacao.nextElementSibling.style.display = 'block'
-  spanOperacao.style.color = '#FF0000'
-}
 
 hello();
--- Financas/Code/Scripts/login.js
+++ Financas/Code/Scripts/login.js
@@ -2,22 +2,39 @@ const email = document.querySelector('#email')
 const senha = document.querySelector('#senha')
 const keys = Object.keys(localStorage)
 
-
-let find
-
 function checkUser(){
+ 
 keys.forEach((key)=>{
   const usuarioStr = localStorage.getItem(key)
-  var usuarioObj = JSON.parse(usuarioStr)
+  let usuarioObj = JSON.parse(usuarioStr)
   if(email.value === usuarioObj.email && senha.value === usuarioObj.senha){
     document.querySelector('#login').style.display = 'none'
     document.querySelector('.loading').style.display = 'block'
+  let logado = {
+    usuario : usuarioObj.nome,
+    logado : true,
+    acess: 0
+  }
+
+ 
+
   setTimeout(function(){
     window.location.href = 'http://127.0.0.1:5500/Pages/home/home.html'
+    logado.acess++
+    localStorage.setItem('logado',JSON.stringify(logado))
+    logado.acess++
+    localStorage.setItem('logado',JSON.stringify(logado))
+    const logadoStorage = localStorage.getItem('logado')
+    const logadoObj = JSON.parse(logadoStorage)
+    if(logadoObj.acess === 1){
+      alert('É a primeira vez no site')
+    }else if(logadoObj.acess>1){
+      alert('Pode mostrar home')
+    }
     },2000)
-    localStorage.setItem('logado',JSON.stringify(usuarioObj))
     }
   })
+
 } 
 document.querySelector('#login').addEventListener('click',checkUser)
 
